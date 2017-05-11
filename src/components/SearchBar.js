5a355a5e2002b86dbe6e6813a83cdf1f461b8c0d
Jonathan Chin <jonathankchin@gmail.com>
2017-05-11 12:31:35 -0400

    fixed COR error?

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,27 +1,40 @@
 import React, {Component} from 'react';
 
-const style = {
-    display: "inline-block"
-}
-
-
-
 export default class SearchBar extends Component {
-    render(){
-        return(
-            <div >
-                <SearchInput placeholder="Search Query" style={style}/>
-                <AdvancedSearch style={style}/>
-            </div>
-        )
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchQuery: ""
+        }
+        this.onInputChange = this
+            .onInputChange
+            .bind(this);
     }
-}
 
+    onInputChange = event => {
+        const target = event.target;
+        const name = target.name;
+        this.setState({[name]: target.value});
+        event.preventDefault();
+    }
 
-class SearchInput extends Component {
-    render(){
-        return(
-          <input placeholder={this.props.placeholder}/>
+    fetchWalmartSearch(query) {
+        fetch(`https://api.walmartlabs.com/v1/search?apiKey=ymvvan7nrev6tffvye9cjdyd&query=${query}s&format=json`)
+            .then(response => response.json())
+            .then(result => console.log(result));
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+    }
+
+    render() {
+        return (
+            <div >
+                <SearchInput placeholder="Search Query"/>
+                <AdvancedSearch/>
+            </div>
         )
     }
 }
@@ -31,8 +44,14 @@ class AdvancedSearch extends Component {
         return (
             <span>
                 <SearchInput placeholder="Brand Name"/>
-                <SearchInput placeholder="Results" />
+                <SearchInput placeholder="Results"/>
             </span>
         )
+    }
+}
+
+class SearchInput extends Component {
+    render() {
+        return (<input placeholder={this.props.placeholder}/>)
     }
 }
