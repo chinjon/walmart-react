726a2d8a319b845f2fc5a6450f254f21e4888fa7
chinjon <Jonathankchin@gmail.com>
2017-05-11 00:34:41 -0400

    remove semantic for react boostrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import { Input } from 'semantic-ui-react';
 
-import axios from 'axios';
 
-import MenuBar from './components/MenuBar';
+
+
 
 class App extends Component {
 
@@ -15,31 +14,13 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {
-    axios.get(`https://api.walmartlabs.com/v1/search?apiKey=ymvvan7nrev6tffvye9cjdyd&&query=jeans&format=json`)
-    .then(res =>{
-      console.log("axios fired");
-      console.log(res.data)
-      this.setState({items: res.data})
-    })
-  }
-
-
   render() {
     return (
       <div>
-        <MenuBar />
+       
       </div>
     );
   }
 }
-
-
-const SearchInput = () =>(
-  <Input focus placeholder="Search..." />
-)
-
-
-
 
 export default App;
