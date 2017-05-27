29b7c19e416c4f85fac9fb99579b98c6b0fa2a6e
Jonathan <jonathankchin@gmail.com>
2017-05-27 13:16:32 -0400

    cleaned up utils and sstarted work on improved table

--- src/components/utils/helpers.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-const api = require('./../../../server/routes/walmart-api');
-
-const helpers = {
-    getSearch: (query, brandName, results, startAt, sortedBy) =>{
-        console.log('search helper is running');
-    }
-}
