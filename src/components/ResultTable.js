850289dbb46c9519401b12a7578cac1e21281ea2
Jonathan <jonathankchin@gmail.com>
2017-07-05 22:38:20 -0400

    added result table

--- src/components/ResultTable.js
+++ src/components/ResultTable.js
@@ -1,1 +1,15 @@
 import React, {Component} from 'react';
+
+import ReactTable from 'react-table';
+
+class ResultTable extends Component {
+    constructor(props,context) {
+        super(props,context)
+    }
+    render() {
+        return(
+            <div>
+            </div>
+        )
+    }
+}
